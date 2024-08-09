fc54924fe9533e04a76366b45363c9203f9b5b66
Aberta Sistemas <DiogoAnderle>
2024-08-09 12:08:56 -0300

    chore: add next auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image"
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetContent, SheetClose, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/quickSearch"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogContent,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
-  const avatarImageUrl =
-    "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww"
+  // const avatarImageUrl =
+  //    "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww"
 
   return (
     <SheetContent className="overflow-y-auto">
@@ -16,14 +24,40 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="p-y flex items-center gap-3 border-b border-solid">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                alt="Fazer Login com o Google"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src={avatarImageUrl}></AvatarImage>
         </Avatar>
         <div>
           <p className="font-bold">Diogo Anderle</p>
           <p className="text-xs">anderle88@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
